bba3a97df0aa3ee3e156c1e0163879f48e15d8eb
Enrique Juan de Dios <juandedioz@gmail.com>
2019-10-12 13:03:54 -0500

    [#1] Setup React, Tree Map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+import users from "./data/user";
+
+const getItems = (acc, obj, items, id) => {
+  if (obj.manager === id) {
+    obj.children = getTree(items, obj.id);
+    acc.push(obj);
+  }
+  return acc;
+};
+
+const getTree = (items, manager) => {
+  const mapper = [];
+  for (let i = 0, t = items.length; i < t; i++) {
+    const item = items[i];
+    if (item.manager === manager) {
+      item.children = items.reduce(
+        (acc, obj) => getItems(acc, obj, items, item.id),
+        []
+      );
+      mapper.push(item);
+    }
+  }
+  return mapper;
+};
+
+const Card = ({ user }) => {
+  return (
+    <div>
+      <div>
+        Card Player: {user.first} {user.last}
+      </div>
+      <div>Manager: {user.manager}</div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 function App() {
+  console.log("TREES", getTree(users, 0));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, index) => (
+        <Card key={index} user={user} />
+      ))}
     </div>
   );
 }
