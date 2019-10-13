1a59bbf1122cb82f78c73537085931de10c2d991
Enrique Juan de Dios <juandedioz@gmail.com>
2019-10-13 10:09:33 -0500

    [#4] Implement recursive API request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import users from "./data/user";
+import React, { useState, useEffect } from "react";
 import Card from "./components/Card";
-import { getTree } from "./utils/parsers";
+import { getTree, getPeople } from "./utils/parsers";
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    getPeople(setPeople);
+  }, []);
+
   return (
     <div className="App">
-      {getTree(users, 0).map(user => (
+      {getTree(people, 0).map(user => (
         <div key={`m-${user.id}`}>
           <Card user={user}></Card>
         </div>
